04a67509bef267e0cebff396fde5cfa2399ab8c0
LeticiaOchoa <88337103+LeticiaOchoa@users.noreply.github.com>
2022-09-25 18:49:47 -0600

    Update PEI.controller.js
    agregue update PEI en controladores

--- controllers/PEI.controller.js
+++ controllers/PEI.controller.js
@@ -3,24 +3,22 @@ const config = require("../config/auth.config");
 const { request, response } = require('express');
 const { Op, DataTypes, Model } = require("sequelize");
 
-const new_empleado = async (req,res) =>{
-    try{
-        //db.sequelize.authenticate();
-        db.empleado.create({
-            dni: req.body.dni,
-            nombre: req.body.nombre,
-            apellido: req.body.apellido,
-            direccion: req.body.direccion,
-            telefono : req.body.telefono,
-            fechaNacimiento : req.body.fecha_nacimiento,
-            sexo: req.body.sexo
-        });
-        return res.status(200).json({status:"ok"});
+const updatePEI = async(req, res) => {
+    try {
+
+      const PEI = await db.PEI.findByPk(req.body.id);
+      if(!PEI){
+        return res.status(404).send({message:'PEI not found'})
+      }
+      await db.PEI.update({name:req.body.name, initialYear: req.body.initialYear, finalYear: req.body.finalYear},{where:{id:req.body.id}})
+      return res.status(200).send(user);
+
     } catch(error){
-        console.log("error: " + error);
-        return res.status(400).json({status:"error", error : error});
+      res.status(500).json({
+        message:'error al ingresar ' + error
+      })
     }
-}
+  }
 
 const get_PEI = async (req,res) =>{
     try{
@@ -36,7 +34,6 @@ const get_PEI = async (req,res) =>{
 
 
 module.exports = {
-    new_empleado,
-    get_empleado_by_id,
+    updatePEI,
     get_PEI
   }
