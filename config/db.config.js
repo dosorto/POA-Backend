df256aaf71dd63b8ec138510981fc41e8fa04674
alejandra Fortin <88343738+alejandrafortin@users.noreply.github.com>
2022-09-27 14:42:44 -0600

    SYSCURLP-9//CONTROLADOR CONTRASEÑA

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const pass = require("./auth.config")
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: pass.pass,
+    PASSWORD: "ANFAalejandra192000",
     DB: "db_poa",
     dialect: "mysql",
     pool: {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,7 +14,9 @@ module.exports = function(app) {
   app.get("/auth/get-all",controller.allUser);
   app.post("/auth/create-user",controller.newUser);
   app.post("/auth/changePassword",controller.changePassword);
-  
+
 };
+
+
 
 
