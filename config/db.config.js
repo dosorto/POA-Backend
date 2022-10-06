9603487b993d11f739c4cb54a1978dae9e7d6c02
David Mendoza <david.mendoza@unah.hn>
2022-10-06 08:46:28 -0600

    Merge desde rama jaime, ultima presentacion

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ const pass = require("./auth.config")
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD:"",
+    PASSWORD:"22@Soporte",
     DB: "db_poa",
     dialect: "mysql",
     pool: {
