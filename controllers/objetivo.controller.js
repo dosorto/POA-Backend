0f9219cb62c403401739e56ac41f2b818c5c5eaf
onielos <onielosespinal@gmail.com>
2022-10-29 22:36:13 -0600

    objetivos modificaciones

--- controllers/objetivo.controller.js
+++ controllers/objetivo.controller.js
@@ -17,13 +17,58 @@ const AllObjetivo = async(req,res) => {
          model: db.dimension
       }]
     })
-      return res.status(200).send({ allObjetivo });
+    res.status(200).json( allObjetivo );
   } catch(error){
       res.status(400).json({
         message:'error al ingresar' + error
       })
   }
   };
+
+  const AllObjetivo_by_idDimension = async(req,res) => { 
+    try{ 
+      const allObjetivo =  await db.objetivos.findAll({
+      where: {
+          isDelete: false,
+          idDimension: req.params.idDimension
+      },
+      include:[{
+        model: db.pei,
+      },{
+         model: db.dimension
+      }]
+    })
+    res.status(200).json( allObjetivo );
+  } catch(error){
+      res.status(400).json({
+        message:'error al ingresar' + error
+      })
+  }
+  };
+
+  const AllObjetivo_by_id = async(req,res) => { 
+    try{ 
+      const allObjetivo =  await db.objetivos.findOne({
+      where: {
+          isDelete: false,
+          id: req.params.id
+      },
+      include:[{
+        model: db.pei,
+      },{
+         model: db.dimension
+      }]
+    })
+    res.status(200).json( allObjetivo );
+  } catch(error){
+      res.status(400).json({
+        message:'error al ingresar' + error
+      })
+  }
+  };
+
+
+
 
   const newObjetivo = async (req, res) => {
     try {
@@ -87,5 +132,7 @@ module.exports = {
   AllObjetivo,
   eliminarObjetivo,
   newObjetivo,
-  updateObjetivo
+  updateObjetivo,
+  AllObjetivo_by_idDimension,
+  AllObjetivo_by_id
 }
