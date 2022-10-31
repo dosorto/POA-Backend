57c90cfd3a761880aaa67f80245b6591f337cac2
jaimeluna24 <jaimeluna600@gmail.com>
2022-10-31 13:57:04 -0600

    Merge del controlador de resultados

--- models/resultados.model.js
+++ models/resultados.model.js
@@ -4,6 +4,10 @@ module.exports = (sequelize, Sequelize) =>{
            type: Sequelize.STRING,
            allowNull: false
         },
+        descripcion:{
+            type: Sequelize.STRING,
+            allowNull: false
+        },
         isDelete: {
             type: Sequelize.BOOLEAN,
             allowNull: false,
