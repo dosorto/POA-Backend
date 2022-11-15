20146ae9e3cd92eb1019cd8484e4c9da06203895
jaimeluna24 <jaimeluna600@gmail.com>
2022-11-14 23:23:46 -0600

    Merge de indicadores POA

--- config/init.config.js
+++ config/init.config.js
@@ -640,20 +640,21 @@ exports.initial = async () => {
             idUE: 1
         });
 
+        //Indicadores_Poa
+        await db.indicadoresPoa.create ({
+            nombre : "Indicador 1",
+            descripcion : "Indicador 1",
+            cantidadPlanificada : 20,
+            cantidadEjecutada : 0,
+            promedioAlcanzado : 0,
+            idActividad: 1
+
+        })
 
     } catch (error) {
         console.log(error);
     }
 
-    //Indicadores_Poa
-    await db.indicadoresPoa.create ({
-        nombre : "Indicador 1",
-        descripcion : "Indicador 1",
-        cantidadPlanificada : 20,
-        cantidadEjecutada : 0,
-        promedioAlcanzado : 0,
-        idActividad: 1
-
-    })
+    
 
 };
