541e4eb336cb6605832e329b23dec21926aff4a5
onielos <onielosespinal@gmail.com>
2022-12-10 16:51:30 -0600

    controladores de reportes y rutas modificadas

--- routes/reportes.routes.js
+++ routes/reportes.routes.js
@@ -12,5 +12,14 @@ module.exports = function(app) {
   app.get("/reportes/get_all/:id", controller.get_all_departamento);
   app.get("/reportes/get_all_depto/:idDepto/:id", controller.get_all_poa_by_idDepto);
   app.get("/reportes/get_all_depto_poa/:idPoa/:idDepto", controller.AllTarea_by_depto_poa);
-
+  app.get("/reportes/suma11/:idPoa", controller.suma_Fuente11);
+  app.get("/reportes/suma12/:idPoa", controller.suma_Fuente12);
+  app.get("/reportes/suma12B/:idPoa", controller.suma_Fuente12B);
+  app.get("/reportes/nopresupuesto/:idPoa", controller.Tareas_sin_presupuesto);
+  app.get("/reportes/presupuesto/:idPoa", controller.Tareas_con_presupuesto);
+  app.get("/reportes/estado_Formulacio/:idPoa", controller.Actvidades_estadoF);
+  app.get("/reportes/estado_ReFormulacion/:idPoa", controller.Actvidades_estadoRF);
+  app.get("/reportes/estado_Revision/:idPoa", controller.Actvidades_estadoR);
+  app.get("/reportes/estado_Aprobada/:idPoa", controller.Actvidades_estadoA);
+  app.get("/reportes/estado_Rechazada/:idPoa", controller.Actvidades_estadoREC);
 };
